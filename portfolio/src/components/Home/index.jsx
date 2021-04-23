eb8cbbd20092473d128f29aefae6c0294ba51057
Norbu Sherpa <norbuts@hotmail.com>
2021-04-23 13:35:14 +1000

    Added image and styling

--- portfolio/src/components/Home/index.jsx
+++ portfolio/src/components/Home/index.jsx
@@ -1,9 +1,24 @@
+import { Link } from "react-router-dom";
 import React from 'react'
 
  const Home = () => {
     return (
-        <div>
-            Home component   
+        <div className="home">
+            <div className ="title">
+							<h1>
+								<p> Hi, </p>
+								<p> I am Norbu</p>
+								<p> Salesforce Engineer</p>
+							</h1>
+							<Link to="about">
+								<button>More Info</button>
+							</Link>
+						</div>
+						<div className="person">
+							<img
+							src={`${process.env.PUBLIC_URL}/Self.jpeg`}
+							alt="photo"/>
+						</div>
         </div>
     )
 };
